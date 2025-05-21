6ea18282905c833abe3e063e24d1667f9f1a57e3
Léo <leo.limousin01@gmail.com>
2025-05-21 10:11:27 +0200

    fini + corrigé

--- front/src/layout/AppSidebar.tsx
+++ front/src/layout/AppSidebar.tsx
@@ -42,15 +42,14 @@ const navItems: NavItem[] = [
     path: "/calendar",
   },
   {
-    icon: <UserCircleIcon />,
-    name: "User Profile",
-    path: "/profile",
-  },
-
-  {
     name: "Forms",
     icon: <ListIcon />,
     subItems: [{ name: "Form Elements", path: "/form-elements", pro: false }],
+  },
+  {
+    icon: <PageIcon />, 
+    name: "Infos",
+    path: "/infos", 
   },
   {
     name: "Tables",
