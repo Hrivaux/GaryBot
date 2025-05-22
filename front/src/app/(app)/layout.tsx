a27a9ec764b44e94f8a001567bf71d0cfc4ff780
Louis Borroni <louis.borroni@yahoo.com>
2025-05-22 15:27:27 +0200

    erreurs console

--- front/src/app/(app)/layout.tsx
+++ front/src/app/(app)/layout.tsx
@@ -35,28 +35,22 @@ export default function RootLayout({
   const shouldShowFloatingChat = pathname !== '/garybot';
 
   return (
-    <html lang="fr">
-      <body className={`${outfit.className} dark:bg-gray-900`}>
-        <ThemeProvider>
-          <SidebarProvider>
-            <ChatProvider>
-              <ChatRouteHandler />
+    <ThemeProvider>
+      <SidebarProvider>
+        <ChatProvider>
+          <ChatRouteHandler />
 
-              {shouldShowSidebar && <AppSidebar />}
+          {shouldShowSidebar && <AppSidebar />}
 
-              <main
-                className={`transition-all ${
-                  shouldShowSidebar ? 'lg:ml-[290px]' : ''
-                }`}
-              >
-                {children}
-              </main>
+          <main
+            className={`transition-all ${shouldShowSidebar ? 'lg:ml-[290px]' : ''} ${outfit.className} dark:bg-gray-900`}
+          >
+            {children}
+          </main>
 
-              {shouldShowFloatingChat && <FloatingChatButton />}
-            </ChatProvider>
-          </SidebarProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+          {shouldShowFloatingChat && <FloatingChatButton />}
+        </ChatProvider>
+      </SidebarProvider>
+    </ThemeProvider>
   );
 }
